893d9202f073750fe014afadd8b3bcb821006d23
2089488 <2089488@gla.ac.uk>
2015-02-23 00:36:27 +0000

    adds customising to admin pages; chapter 2 completed

--- polls/models.py
+++ polls/models.py
@@ -7,7 +7,19 @@ class Question(models.Model):
     question_text = models.CharField('question', max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
+    def __unicode__(self):
+        return self.question_text
+
 class Choice(models.Model):
     question_text = models.ForeignKey(Question)
     choice_text = models.CharField('choice', max_length=200)
     votes = models.IntegerField('number of votes:', default=0)
+
+    def __unicode__(self):
+        return self.choice_text
